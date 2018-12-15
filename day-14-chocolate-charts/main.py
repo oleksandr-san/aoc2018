62650e09585247eaa1021593706290bdd757f5f2
Alexander Anosov <alexanderanosov1996@gmail.com>
2018-12-15 12:17:39 +0200

    Smol fixes in day 14

--- day-14-chocolate-charts/main.py
+++ day-14-chocolate-charts/main.py
@@ -5,8 +5,8 @@ def eval_recipes(shift, cnt):
     recipes, elves = [3, 7], [0, 1]
 
     while len(recipes) < (shift + cnt):
-        recipes.extend(digits(sum(recipes[e] for e in elves)))
-        elves = list((e + recipes[e] + 1) % len(recipes) for e in elves)
+        recipes.extend(digits(sum(recipes[elf] for elf in elves)))
+        elves = [(elf + recipes[elf] + 1) % len(recipes) for elf in elves]
 
     return recipes[shift:shift+cnt]
 
@@ -14,14 +14,14 @@ def eval_position(seq):
     recipes, elves = [3, 7], [0, 1]
 
     while True:
-        for digit in digits(sum(recipes[e] for e in elves)):
+        for digit in digits(sum(recipes[elf] for elf in elves)):
             recipes.append(digit)
             if recipes[-len(seq):] == seq:
                 return len(recipes) - len(seq)
 
-        elves = list((e + recipes[e] + 1) % len(recipes) for e in elves)
+        elves = [(elf + recipes[elf] + 1) % len(recipes) for elf in elves]
 
 input = "327901"
 
-print('Part 1:', ''.join(str(v) for v in eval_recipes(int(input), 10)))
-print('Part 2:', eval_position(list(int(c) for c in input)))
+print('Part 1:', ''.join(map(str, eval_recipes(int(input), 10))))
+print('Part 2:', eval_position(list(map(int, input))))
